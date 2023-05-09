4b12dd71638d384bdcc0982362a570cc254282ae
Greg Reda <gjreda+github@gmail.com>
2023-05-09 15:36:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# haystack pdf chatbot
+
+Prototyping a bot that allows for chat over PDF documents.
+
+Note that this uses BM25 for document retrieval, rather than a semantic approach with embeddings.
+
+## Setup
+This uses [Poetry](https://python-poetry.org/) for dependency management. To install dependencies:
+```bash
+$ poetry install
+```
+
+You'll also need to create a `.env` file and add your `OPENAI_API_KEY` to it (see `.env.example`).
+
+## Usage
+The command below will run the pipeline on the `papers` directory, which contains a few PDFs. It will then start a REPL where you can ask questions about the PDFs. You can exit the Q&A loop by typing "exit" or cmd/ctrl + c
+```python
+$ poetry run python haystack_pdf_bot/main.py --pdf_directory=papers
+```
---
+++ haystack_pdf_bot/main.py
@@ -0,0 +1,66 @@
+from haystack.document_stores import InMemoryDocumentStore
+from haystack.nodes import PDFToTextConverter, PreProcessor, BM25Retriever, PromptNode, PromptTemplate
+from haystack.pipelines import Pipeline
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def main(pdf_directory: str):
+    text_extractor = PDFToTextConverter(remove_numeric_tables=True)
+    preprocessor = PreProcessor(
+        split_by='word',
+        split_respect_sentence_boundary=True,
+        split_length=200,
+        split_overlap=20,
+    )
+    document_store = InMemoryDocumentStore(use_bm25=True)
+
+    injest = Pipeline()
+    injest.add_node(text_extractor, name="text_extractor", inputs=["File"])
+    injest.add_node(preprocessor, name="preprocessor", inputs=["text_extractor"])
+    injest.add_node(document_store, name="document_store", inputs=["preprocessor"])
+
+    path = Path(pdf_directory)
+    files = list(path.glob('*.pdf'))
+    files_metadata = [{"name": file.name, "path": file} for file in files]
+    injest.run(file_paths=files, meta=files_metadata)
+
+    retriever = BM25Retriever(document_store=document_store, top_k=5)
+    prompt = PromptTemplate(
+        name="question",
+        prompt_text="""Answer the question based on the context below.
+        Context: {join(documents)}\n\n
+        Question: {query}\n\n
+        Answer:"""
+    )
+    prompt_node = PromptNode(model_name_or_path="text-davinci-003",
+                             api_key=os.environ["OPENAI_API_KEY"], default_prompt_template=prompt)
+    chatbot = Pipeline()
+    chatbot.add_node(retriever, name="retriever", inputs=["Query"])
+    chatbot.add_node(prompt_node, name="prompt", inputs=["retriever"])
+
+    while True:
+        query = input("Enter a question: ")
+
+        if query == "exit":
+            break
+
+        response = chatbot.run(query=query)
+        print(response['results'])
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    from pathlib import Path
+
+    parser = ArgumentParser()
+    parser.add_argument('--pdf_directory', help='Path to directory containing PDFs to ingest')
+    args = parser.parse_args()
+
+    path = Path(args.pdf_directory)
+    if not path.exists():
+        raise ValueError(f"Directory {path} does not exist")
+
+    main(pdf_directory=path.name)
